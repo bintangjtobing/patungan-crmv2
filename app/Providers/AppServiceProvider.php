f5adb50a7699f200ae74b1b0b2ef9202b1f584a7
Bintang Tobing <hello@bintangtobing.com>
2024-11-23 19:33:58 +0700

    update: env production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use Illuminate\Support\ServiceProvider;
 use App\Observers\KredentailObserver;
 use App\Models\KredentialCustomer;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,9 +24,6 @@ class AppServiceProvider extends ServiceProvider
     {
         KredentialCustomer::observe(KredentailObserver::class);
 
-    // Cek apakah koneksi database tersedia
-    if (Schema::hasTable('kredential_customers')) {
-            DB::statement("SET SESSION sql_mode=(SELECT REPLACE(@@SESSION.sql_mode, 'ONLY_FULL_GROUP_BY', ''));");
-        }
+        DB::statement("SET SESSION sql_mode=(SELECT REPLACE(@@SESSION.sql_mode, 'ONLY_FULL_GROUP_BY', ''));");
     }
 }
