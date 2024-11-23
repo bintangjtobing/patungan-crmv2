f89c88da967e85262bda8b456b8633c84d569b42
Bintang Tobing <hello@bintangtobing.com>
2024-11-23 19:37:12 +0700

    update: app service php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
     {
         KredentialCustomer::observe(KredentailObserver::class);
 
-        DB::statement("SET SESSION sql_mode=(SELECT REPLACE(@@SESSION.sql_mode, 'ONLY_FULL_GROUP_BY', ''));");
+        // DB::statement("SET SESSION sql_mode=(SELECT REPLACE(@@SESSION.sql_mode, 'ONLY_FULL_GROUP_BY', ''));");
     }
 }
